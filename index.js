31139578f53134bedfb51f6d14ced472061437d6
Vitor_Hugo <40009988+TorugoH@users.noreply.github.com>
2020-10-30 18:20:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-let larguraPagina=window.innerWidth
-console.log(larguraPagina)
 
-function escrever(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ''
-    textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 150 * i);
-    })
-  }
-  
-  const s=document.querySelector('.s')
-  const u=document.querySelector('.u')
-  const b=document.querySelector('.b')
-  const tituloPrincipal=document.querySelector('span')
-  const titulo = document.querySelector('h2')
-      if(larguraPagina<=760){
-        escrever(titulo)
-        escrever(s)
-        escrever(u)
-        escrever(b)
+/*Carrosel*/
+  let fotos=["calculadora", "blog","conversor","campinf","integrando","mosquito","portifolio","quimica","sitefacu","siteRock","velha"];
+  let btn_proximo= document.getElementById("proximo");
+  let imagens= document.createElement("img");
+   imagens.src = "imagens/despesa.png";
+   let car = document.getElementById("carrosel");
+   car.appendChild(imagens);
+      let i=-1;
+   const proximo=()=>{
+      if(i<fotos.length-1){
+        i++
+        imagens.src  = "imagens/"+ fotos[i]+".png";
+        document.getElementById("carrosel").appendChild(imagens);
       }
-      else{
-        escrever(tituloPrincipal)
-        escrever(titulo)
-        escrever(s)
-        escrever(u)
-        escrever(b)
+      else if(i>=fotos.length-1){
+        imagens.src="imagens/despesa.png";
+        console.log(imagens.src)
+        document.getElementById("carrosel").appendChild(imagens);
+        i=0;
       }
+    }
+    /*mexer dps*/
+    let btn_anterior=document.getElementById("anterior");
+    const anterior=()=>{
+      if(i>=0||i<=fotos.length-1){
+        i--
+        imagens.src  = "imagens/"+ fotos[i]+".png";
+        document.getElementById("carrosel").appendChild(imagens);
+        console.log(i)
+      }
+      if(i<=-1){
+        i=0
+        imagens.src="imagens/despesa.png";
+        console.log(imagens.src)
+        document.getElementById("carrosel").appendChild(imagens);
+      }
+    }
+      btn_proximo.addEventListener("click",proximo);
+      btn_anterior.addEventListener("click",anterior)
+      window.setInterval(proximo,5000);
